aff7cd9c14393bd0eb8852035ae675baa5d94284
phil <phil@LenovoLark>
2018-05-04 18:24:42 -0700

    subject done

--- APCS_Final_Project/src/Events/Subject.java
+++ APCS_Final_Project/src/Events/Subject.java
@@ -1,5 +1,38 @@
 package Events;
 
 public class Subject {
+	
+	private String subjectName;
+	private double gradePercentage;
+	
+	public Subject(String subjectName, double gradePercentage) {
+		this.subjectName = subjectName;
+		this.gradePercentage = gradePercentage;
+	}
+	
+	public String getSubjectName() {
+		return subjectName;
+	}
+	
+	public double getGradePercentage() {
+		return gradePercentage;
+	}
+	
+	public String getLetterGrade() {
+		if(gradePercentage >= 90) 
+			return "A";
+		else if(gradePercentage >= 80)
+			return "B";
+		else if(gradePercentage >= 70)
+			return "C";
+		else if(gradePercentage >= 60)
+			return "D";
+		else
+			return "F";
+	}
+	
+	public String toString( ) {
+		return "Subject: " + subjectName + " Grade: " + getLetterGrade();
+	}
 
 }
