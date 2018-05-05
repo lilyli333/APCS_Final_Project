3e4d792f1cee4a129075e130fa2580d393d2b2c5
speedy <speedy@10.0.1.43>
2018-05-04 18:18:14 -0700

    Assignment updated

--- APCS_Final_Project/src/Assignment/Assignment.java
+++ APCS_Final_Project/src/Assignment/Assignment.java
@@ -2,4 +2,48 @@ package Assignment;
 
 public class Assignment {
 
+	private int timeLimit;
+	private boolean isCompleted;
+	private int pointsPossible;
+	private String title;
+	
+	public Assignment(int time, boolean isDone, int points, String name)
+	{
+		timeLimit = time;
+		isCompleted = isDone;
+		pointsPossible = points;
+		title = name;
+	}
+
+	public int getTimeLimit() {
+		return timeLimit;
+	}
+
+	public void setTimeLimit(int timeLimit) {
+		this.timeLimit = timeLimit;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+	public int getPointsPossible() {
+		return pointsPossible;
+	}
+
+	public void setPointsPossible(int pointsPossible) {
+		this.pointsPossible = pointsPossible;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
