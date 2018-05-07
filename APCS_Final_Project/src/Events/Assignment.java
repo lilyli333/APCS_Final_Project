edecf85a957625fc987c3d1013c66bafd575e39e
hwang635 <hwang635@PR10.hhs.win.fuhsd.org>
2018-05-07 13:54:34 -0700

    assignment package deleted

--- APCS_Final_Project/src/Events/Assignment.java
+++ APCS_Final_Project/src/Events/Assignment.java
@@ -2,4 +2,50 @@ package Events;
 
 public class Assignment {
 
+	private int timeLimit;
+	private boolean isCompleted;
+	private String title;
+	private int priority;
+	private int pointsPossible;
+	
+	public Assignment(int time, boolean isDone, String name, int stars)
+	{
+		timeLimit = time;
+		isCompleted = isDone;
+		title = name;
+		priority = stars;
+		pointsPossible = stars * 10;
+	}
+
+	public int getTimeLimit() {
+		return timeLimit;
+	}
+
+	public void setTimeLimit(int timeLimit) {
+		this.timeLimit = timeLimit;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+	public int getPointsPossible() {
+		return pointsPossible;
+	}
+
+	public void setPointsPossible(int pointsPossible) {
+		this.pointsPossible = pointsPossible;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
